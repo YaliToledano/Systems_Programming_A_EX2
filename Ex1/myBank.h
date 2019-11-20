90290456c812305117ae99be4d6f8915eff1a15b
Live session user <you@example.com>
2019-11-20 09:51:51 +0000

    first init Ex1

---
+++ Ex1/myBank.h
@@ -0,0 +1,17 @@
+#ifndef _MYLIB_H_
+#define _MYLIB_H_
+
+
+float add(float x, float y);
+
+float sub(float x, float y);
+
+double mul(double x, int y);
+
+double div(double x, int y);
+
+double Exp(int x);
+
+double Pow(double x, int y);
+
+#endif
